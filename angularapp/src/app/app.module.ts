eea57cd1d249183039b17b2a681a1f4901be2911
Iamneo <techadmin@iamneo.ai>
2023-07-31 08:27:24 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { } from form '@angular/forms'
+import {formsModule } from form '@angular/forms'
 
 @NgModule({
   declarations: [
