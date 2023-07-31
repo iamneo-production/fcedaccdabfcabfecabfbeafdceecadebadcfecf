f58969df90857f90f3f86be23ce24f887a3c27bf
Iamneo <techadmin@iamneo.ai>
2023-07-31 09:05:58 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl.Userregistration} from '.angular/forms'
+import {FormControl,FormGroup} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import {FormControl.Userregistration} from '.angular/forms'
 })
 export class AppComponent {
   title = 'angularapp';
+UserRegistration: FormGroup<any>;
 }
