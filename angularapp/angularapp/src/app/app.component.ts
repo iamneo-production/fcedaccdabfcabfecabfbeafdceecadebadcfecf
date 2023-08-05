f12cfeabd2edfc21bcf6b2280b11ac4023edcc60
Iamneo <techadmin@iamneo.ai>
2023-08-05 15:43:33 +0000

    Backup Commit

--- angularapp/angularapp/src/app/app.component.ts
+++ angularapp/angularapp/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.css'],
 })
-export class AppComponent {
-  title = 'angularapp';
+export class UserRegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+$')]],
+      gender: ['', Validators.required],
+      country: ['', Validators.required],
+      city: ['', Validators.required],
+      age: ['', []],
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      // Process the form data here
+      console.log('Form submitted:', this.registrationForm.value);
+    }
+  }
+
+  isFieldInvalid(field: string): boolean {
+    const control = this.registrationForm.get(field);
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  isAgeRequired(): boolean {
+    const countryControl = this.registrationForm.get('country');
+    return ['US', 'Canada', 'India'].includes(countryControl.value);
+  }
 }
--- angularapp/angularapp/src/app/app.module.ts
+++ angularapp/angularapp/src/app/app.module.ts
@@ -1,18 +1,14 @@
+mport { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; // Add this line
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, UserRegistrationComponent],
+  imports: [BrowserModule, ReactiveFormsModule], // Add ReactiveFormsModule here
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
