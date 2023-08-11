842e9738dae3cfc775eba81bba710c8afb0eb937
Iamneo <techadmin@iamneo.ai>
2023-08-11 10:54:29 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,38 +1,36 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   registrationForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.registrationForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]+$')]],
-      gender: [''],
+      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^[a-zA-Z]+$')]],
+      gender: ['', Validators.required],
       country: ['', Validators.required],
       city: ['', Validators.required],
-      age: ['', []],
+      age: ['']
     });
   }
 
-  // Helper function to check if a form field is invalid and has been touched or dirty
   isFieldInvalid(field: string): boolean {
-    const control: AbstractControl | null = this.registrationForm.get(field);
-    return control!.invalid && (control!.dirty || control!.touched) || false;
+    const control = this.registrationForm.get(field);
+    return control!.invalid && (control!.dirty || control!.touched);
   }
 
-  // Helper function to check if age is required based on the selected country
   isAgeRequired(): boolean {
-    const countryControl: AbstractControl | null = this.registrationForm.get('country');
-    return ['US', 'Canada', 'India'].includes(countryControl!.value);
+    const countryControl = this.registrationForm.get('country');
+    return ['US', 'CA', 'IN'].includes(countryControl!.value);
   }
 
-  // Function to handle form submission
   onSubmit() {
     if (this.registrationForm.valid) {
       console.log('Form submitted:', this.registrationForm.value);
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; // Add this line
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule // Add this line
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
